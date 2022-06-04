17c8ab8cd0053ff39bbf1099ace405e1f9fdb8f6
Brahm <brahm.kelly@gmail.com>
2022-06-04 18:26:37 -0400

    needs styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+
+import "./App.css";
 
 function App() {
+  const api = {
+    key: "a3e733bce38445cf27e1d774a0f0ddd6",
+  };
+  const [weather, setWeather] = useState({});
+  const [locations, setLocations] = useState("london");
+  const [photos, setPhotos] = useState([]);
+  useEffect(() => {
+    ifClicked();
+  }, []);
+
+  function ifClicked() {
+    fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${locations}&APPID=a3e733bce38445cf27e1d774a0f0ddd6&units=metric`
+    )
+      .then((res) => {
+        if (res.ok) {
+          console.log(res.status);
+          return res.json();
+        } else {
+          if (res.status === 404) {
+            return alert("Try another location");
+          }
+          alert("An error occurred");
+          throw new Error("You have an error");
+        }
+      })
+      .then((object) => {
+        setWeather(object);
+        console.log(weather);
+      })
+      .catch((error) => console.log(error));
+    fetch(
+      `https://api.unsplash.com/search/photos?query=${locations}&client_id=fV7Hm0Rk-Op8OgKL6DJZSbTcMdaS8ybrGubIcgOJ72U`
+    )
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          throw new Error("error");
+        }
+      })
+      .then((data) => {
+        console.log(data);
+        setPhotos(data?.results[0]?.urls?.raw);
+      })
+      .catch((error) => console.log(error));
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="wrapper">
+        <div className="search">
+          <input
+            type="text"
+            value={locations}
+            onChange={(e) => setLocations(e.target.value)}
+            placeholder="Enter location"
+            className="location_input"
+          />
+          <button className="location_searcher" onClick={ifClicked}>
+            Search Location
+          </button>
+        </div>
+        <div className="app__data">
+          <p className="temp">Current Temperature in: {weather?.main?.temp}</p>
+        </div>
+        <img className="app__image" src={photos} alt="" />
+      </div>
     </div>
   );
 }
